8d445f676455bcaa2d7149d2c3df0b195b2aa4c9
ramazan yaraliev <yaru@bk.ru>
2018-04-21 22:34:35 +0300

    add simple tests for link and button

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
-RSpec.feature "Visiting the home page", type: :feature do
-  scenario "The visitor should see a 'Select a Chat'" do
+RSpec.feature "Visiting the root page", type: :feature do
+  scenario "The visitor should see a 'Welcome'" do
     visit root_path
     expect(page).to have_text("Welcome")
+  end
+
+  scenario "The visitor should see a link 'Подключиться'" do
+    visit root_path
+    find_link('Подключиться', href: '/chats/connect')
+  end
+
+  scenario "The visitor should see a button 'Создать'" do
+    visit root_path
+    find_button('Создать')
   end
 end
