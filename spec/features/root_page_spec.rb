8f70e5f89d5b940476f2f41d09e51a1f1d11923b
ramazan yaraliev <yaru@bk.ru>
2018-04-22 11:13:10 +0300

    add tests: Page should has input type='text' clicking button/link; edit some tests

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Visiting the root page", type: :feature do
   scenario "The visitor should see a 'Welcome'" do
     visit root_path
-    expect(page).to have_text("Welcome")
+    page.has_content?('Welcome')
   end
 
   scenario "The visitor should see a link 'Подключиться'" do
@@ -13,7 +13,7 @@ RSpec.feature "Visiting the root page", type: :feature do
 
   scenario "The visitor should see a button 'Создать'" do
     visit root_path
-    expect(page).to have_selector("input[type=submit][value='Создать']")
+    page.has_xpath?("input[type=submit][value='Создать']")
   end
 
   scenario "The visitor should see a chat's page after clicking the button 'Создать" do
@@ -22,10 +22,22 @@ RSpec.feature "Visiting the root page", type: :feature do
     expect(page).to have_current_path(chat_path(Chat.first.token))
   end
 
-  scenario "The visitor should see a chat's page after clicking the button 'Создать" do
+  scenario "The visitor should see a chat's page after clicking the link 'Подключиться'" do
     visit root_path
     Chat.create(token: 12345)
     click_link('Подключиться')
     expect(page).to have_current_path(chat_path(Chat.first.token))
+  end
+
+  scenario "Page should has input type='text' after clicking the button 'Создать'" do
+    visit root_path
+    click_button('Создать')
+    page.has_xpath?("//input[@type='text'][class='input-box_text']")
+  end
+
+  scenario "Page should has input type='text' clicking the link 'Подключиться'" do
+    visit root_path
+    click_button('Создать')
+    page.has_xpath?("//input[@type='text'][class='input-box_text']")
   end
 end
