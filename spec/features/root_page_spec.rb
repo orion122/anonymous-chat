20c172f7d33e07aef1a2bdbd8e1c90616b2ef8de
ramazan yaraliev <yaru@bk.ru>
2018-04-22 10:56:15 +0300

    add tests for clicking button and link: expect(page)

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -13,6 +13,19 @@ RSpec.feature "Visiting the root page", type: :feature do
 
   scenario "The visitor should see a button 'Создать'" do
     visit root_path
-    find_button('Создать')
+    expect(page).to have_selector("input[type=submit][value='Создать']")
+  end
+
+  scenario "The visitor should see a chat's page after clicking the button 'Создать" do
+    visit root_path
+    click_button('Создать')
+    expect(page).to have_current_path(chat_path(Chat.first.token))
+  end
+
+  scenario "The visitor should see a chat's page after clicking the button 'Создать" do
+    visit root_path
+    Chat.create(token: 12345)
+    click_link('Подключиться')
+    expect(page).to have_current_path(chat_path(Chat.first.token))
   end
 end
