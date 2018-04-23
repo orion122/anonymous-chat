c340215e65ac6085bda4ee0a25af7915c326311d
ramazan <yaru@bk.ru>
2018-04-23 16:39:50 +0300

    pass sessin token by query string

--- app/assets/javascripts/chats.coffee
+++ app/assets/javascripts/chats.coffee
@@ -1,5 +1,17 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-jQuery ->
-  #alert(session_token)
+$(document).on 'keypress', '.input-box_text', (e) ->
+  $('div[data-session-token]').each ->
+    sessionToken = $(this).data('session-token')
+
+  chat_id = $('.input-box_text').attr('id').split('_')[1]
+
+  if e.keyCode == 13 and e.target.value
+    App.chat.reply({
+      'chat_id': chat_id,
+      'my_name': 'me',
+      'message': e.target.value
+    })
+    e.target.value = ''
+    e.preventDefault()
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,7 @@
 class ChatsController < ApplicationController
   def welcome
-    @session_token = SecureRandom.urlsafe_base64
-    Session.create(token: @session_token)
+    #@session_token = SecureRandom.urlsafe_base64
+    #Session.create(token: @session_token)
 
     @chat = Chat.new
   end
@@ -11,18 +11,26 @@ class ChatsController < ApplicationController
     @chat_token = SecureRandom.urlsafe_base64
     @chat = Chat.create(token: @chat_token)
 
-    redirect_to action: "show", token: @chat_token
+    @session_token = SecureRandom.urlsafe_base64
+    Session.create(token: @session_token)
+
+    redirect_to action: "show", token: @chat_token, session_token: @session_token
   end
 
 
   def show
     @chat = Chat.where(token: params[:token]).first
+
+    @session_token = params[:session_token]
   end
 
 
   def connect
     @random_chat = Chat.where(filled: false).order("RANDOM()").first
 
-    redirect_to action: "show", token: @random_chat.token
+    @session_token = SecureRandom.urlsafe_base64
+    Session.create(token: @session_token)
+
+    redirect_to action: "show", token: @random_chat.token, session_token: @session_token
   end
 end
