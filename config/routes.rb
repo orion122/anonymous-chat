9c1294ef90a881ead6c8a67041052a954058dd5f
ramazan <yaru@bk.ru>
2018-05-21 17:03:42 +0300

    fix spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-      root 'chats#welcome'
+  root 'chats#welcome'
 
-      resources :chats, only: [:create, :show], param: :token do
-        collection do
-          post :join_random
-        end
+  resources :chats, only: [:create, :show], param: :token do
+    collection do
+      post :join_random
+    end
 
-        scope module: :chats do
-        resources :messages, only: [:index, :create]
-      end
+    scope module: :chats do
+      resources :messages, only: [:index, :create]
+    end
   end
 end
