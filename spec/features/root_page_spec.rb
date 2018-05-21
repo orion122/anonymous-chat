af78234650f5fdeb25e2737caf3946e07a87a617
ramazan <yaru@bk.ru>
2018-05-21 16:44:31 +0300

    line length: 80

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -18,25 +18,26 @@ RSpec.feature 'Visiting the root page', type: :feature do
     page.has_xpath?("input[type=submit][value='#{I18n.t('root.create_chat')}']")
   end
 
-  scenario "The visitor should see a chat's page after clicking the button 'Create chat'" do
+  scenario "Visitor should see chat page after clicking 'Create chat'" do
     click_button(I18n.t('root.create_chat'))
     chat = Chat.first
     expect(page).to have_current_path(chat_path(chat.token))
   end
 
-  scenario "The visitor should see a chat's page after clicking the button 'Join random chat'" do
+  scenario "Visitor should see chat page after clicking 'Join random chat'" do
     create(:chat)
     click_button(I18n.t('root.join_random_chat'))
     chat = Chat.first
     expect(page).to have_current_path(chat_path(chat.token))
   end
 
-  scenario "Page should has input type='text' after clicking the button 'Create chat'" do
+  scenario "Page should has input type='text' after clicking 'Create chat'" do
     click_button(I18n.t('root.create_chat'))
     page.has_xpath?("//input[@type='text'][class='input-box_text']")
   end
 
-  scenario "Page should has input type='text' after clicking the button 'Join random chat'" do
+  scenario "Page should has input type='text' after clicking
+            'Join random chat'" do
     create(:chat)
     click_button(I18n.t('root.join_random_chat'))
     page.has_xpath?("//input[@type='text'][class='input-box_text']")
