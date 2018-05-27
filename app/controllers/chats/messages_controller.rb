20d97e9b5c8284c2102e54cd037248623cee597d
ramazan yaraliev <yaru@bk.ru>
2018-05-27 20:21:50 +0300

    before_action :chat_has_session

--- app/controllers/chats/messages_controller.rb
+++ app/controllers/chats/messages_controller.rb
@@ -1,36 +1,33 @@
 class Chats::MessagesController < Chats::ApplicationController
+  before_action :chat_has_session, only: [:index, :create, :set_state_read]
+
   def index
-    if chat_has_session(chat, session_token)
-      change_state(chat.messages, :accepted?, :deliver!)
-      render json: chat.messages
-    else
-      render body: nil, status: :forbidden
-    end
+    change_state(chat.messages, :accepted?, :deliver!)
+    render json: chat.messages
   end
 
   def create
-    if chat_has_session(chat, session_token)
-      session_id = chat.sessions.find_by(token: session_token).id
-      session = Session.find(session_id)
-      message = session.messages.new(message: params[:message])
-      message.accept! if message.save
+    session_id = chat.sessions.find_by(token: session_token).id
+    session = Session.find(session_id)
+    message = session.messages.new(message: params[:message])
+
+    if message.save
+      message.accept!
     else
       render body: nil, status: :internal_server_error
     end
   end
 
   def set_state_read
-    if chat_has_session(chat, session_token)
-      change_state(chat.messages, :delivered?, :read!)
-    else
-      render body: nil, status: :forbidden
-    end
+    change_state(chat.messages, :delivered?, :read!)
   end
 
   private
 
-  def chat_has_session(chat, session_token)
-    chat.sessions.where(token: session_token).exists?
+  def chat_has_session
+    if chat.sessions.where(token: session_token).empty?
+      render body: nil, status: :forbidden
+    end
   end
 
   def change_state(messages, from_state, to_state)
