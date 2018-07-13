80675e0a52b5e437fd5ccdfb9a0ae77c6e1bab85
ramazan <yaru@bk.ru>
2018-07-13 10:54:12 +0300

    add checkSessionTokenFromLocalStorage() for enter_by_session_token to chat

--- app/javascript/packs/components/welcome.vue
+++ app/javascript/packs/components/welcome.vue
@@ -4,13 +4,29 @@
         <h1>{{ $t("root.welcome") }}</h1>
         <button @click="createChat()">{{ $t("root.create_chat") }}</button>
         <button @click="joinRandomChat()">{{ $t("root.join_random_chat") }}</button>
-        {{ rewriteSessionTokenInLocalStorage() }}
+        {{ checkSessionTokenFromLocalStorage() }}
     </div>
 </template>
 
 <script>
     export default {
         methods: {
+            checkSessionTokenFromLocalStorage() {
+                let session_token = localStorage.getItem('session_token')
+                if (session_token !== null) {
+                    this.$http.post('/chats/enter_by_session_token',
+                        {session_token: session_token}
+                    ).then(response => {
+                        if (response.body.session_token_found) {
+                            this.$router.push(`/chats/${response.body.chat_token}`)
+                        } else {
+                            this.rewriteSessionTokenInLocalStorage()
+                        }
+                    });
+                } else {
+                    this.rewriteSessionTokenInLocalStorage()
+                }
+            },
             rewriteSessionTokenInLocalStorage() {
                 localStorage.removeItem('session_token')
                 localStorage.setItem('session_token', gon.session_token)
