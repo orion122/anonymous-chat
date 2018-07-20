5408a7750f6d5ad54e17b7080ad6c14179c560e3
ramazan <yaru@bk.ru>
2018-07-20 11:52:47 +0300

    subscribe to publication of 'create message' event of the current_session_token for getting messages

--- app/javascript/packs/components/chat.vue
+++ app/javascript/packs/components/chat.vue
@@ -18,19 +18,13 @@
             }
         },
         mounted: function(){
-            setInterval(() => {
-                if (window.location.pathname == `/chats/${this.getChatToken()}`) {
-                    this.getMessages()
-                    Rollbar.info("JS: Get all messages after 5 sec")
-                }
-            }, 5000);
-        },
-        methods: {
-            logout() {
-                localStorage.removeItem('session_token')
-                this.$router.push('/')
-            },
-            getMessages() {
+            // setInterval(() => {
+            //     if (window.location.pathname == `/chats/${this.getChatToken()}`) {
+            //         this.getMessages()
+            //         Rollbar.info("JS: Get all messages after 5 sec")
+            //     }
+            // }, 5000);
+            nats.subscribe(this.current_session_token, function(event_description) {
                 this.$http.get(`/chats/${this.getChatToken()}/messages`
                 ).then(response => {
                     let userData = ''
@@ -45,14 +39,36 @@
                     }), [])
                 });
                 Rollbar.info("JS: Get all messages")
+            });
+        },
+        methods: {
+            logout() {
+                localStorage.removeItem('session_token')
+                this.$router.push('/')
             },
+            // getMessages() {
+            //     this.$http.get(`/chats/${this.getChatToken()}/messages`
+            //     ).then(response => {
+            //         let userData = ''
+            //         this.messages = response.body.reduce(((init, messageObject) => {
+            //             userData = `${messageObject.nickname}: ${messageObject.message}`
+            //             if ((messageObject.session_token === this.current_session_token) &&
+            //                 (messageObject.state !== 'delivered')) {
+            //                 userData += ` (${messageObject.state})`
+            //             }
+            //             init.push(userData)
+            //             return init
+            //         }), [])
+            //     });
+            //     Rollbar.info("JS: Get all messages")
+            // },
             saveMessage() {
                 if(this.message !== '') {
                     this.$http.post(`/chats/${this.getChatToken()}/messages`,
                         {message: this.message}
                     ).then(response => {
                         this.message = ''
-                        this.getMessages()
+                        // this.getMessages()
                     });
                     Rollbar.info("JS: Save message")
                 }
