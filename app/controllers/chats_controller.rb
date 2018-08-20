fdc8092790c11e4f60daed354d64c0bbf1634a9f
ramazan yaraliev <yaru@bk.ru>
2018-08-20 20:40:11 +0300

    remove @session_token

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,6 @@
 class ChatsController < ApplicationController
   def welcome
-    @session_token = SecureRandom.uuid
-    gon.session_token = @session_token
+    gon.session_token = SecureRandom.uuid
     @chat = Chat.new
     Rollbar.info('Visiting the main page')
   end
